a2edafa4f5ac0615ff407b7fa1b071c581b7b7df
Jessica-Koch <jessicakoch136@icloud.com>
2017-05-29 21:56:25 -0700

    update navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+    resources :posts
 
-  get 'welcome/about'
+    get 'about' => 'welcome#about'
 
-  get 'welcome/contact'
+    get 'contact' => 'welcome#contact'
 
-  get 'welcome/faq'
+    get 'faq' => 'welcome#faq'
 
-  root 'welcome#index'
+
+
+    root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
