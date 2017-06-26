38bb8e6dd9bf797053f698ae79d832461565784e
Jessica-Koch <jessicakoch136@icloud.com>
2017-06-25 17:53:20 -0700

    users can favorite posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe User, type: :model do
     it {is_expected.to have_many(:comments)}
     it {is_expected.to have_many(:votes)}
     it {is_expected.to have_many(:posts)}
+    it {is_expected.to have_many(:favorites)}
     it {is_expected.to validate_presence_of(:name)}
     it {is_expected.to validate_length_of(:name).is_at_least(1)}
 
@@ -110,6 +111,22 @@ RSpec.describe User, type: :model do
 
         it "returns false for #admin?" do
             expect(user.admin?).to be_falsey
+        end
+    end
+
+    describe "#favorite_for(post)" do
+        before do
+            topic = Topic.create!(name: Faker::Hipster.sentence(3), description: Faker::Hipster.paragraph)
+            @post = topic.posts.create!(title: Faker::Hipster.sentence(3), body: Faker::Hipster.paragraph, user: user)
+        end
+
+        it "returns 'nil' if the user has not favorited the post" do
+            expect(user.favorite_for(@post)).to be_nil
+        end
+
+        it "returns the appropriate favorite if it exists" do
+            favorite = user.favorites.where(post: @post).create
+            expect(user.favorite_for(@post)).to eq(favorite)
         end
     end
 end
