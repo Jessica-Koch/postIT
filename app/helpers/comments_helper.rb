cc6dcf9cafd9782c2655202761a618eafb5cf1da
Jessica-Koch <jessicakoch136@icloud.com>
2017-06-27 23:10:28 -0700

    add user info to show page

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,10 @@ module CommentsHelper
     def user_is_authorized_for_comment?(comment)
         current_user && (current_user == comment.user || current_user.admin?)
     end
+
+    def any_comments?
+        if @user.posts.length == 0
+            "#{@user.name} has not submitted any comments yet."
+        end
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+    def any_posts?
+        if @user.posts.length == 0
+            "#{@user.name} has not submitted any posts yet."
+        end
+    end
 end
